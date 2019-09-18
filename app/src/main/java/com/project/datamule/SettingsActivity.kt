fea0c09e125933eee59ff0ef3ac36c2b7ad458d4
hicham <hicham.akazzab@hva.nl>
2019-09-18 15:58:08 +0200

    added settings ram and storage

--- app/src/main/java/com/project/datamule/SettingsActivity.kt
+++ app/src/main/java/com/project/datamule/SettingsActivity.kt
@@ -2,11 +2,43 @@ package com.project.datamule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.StatFs
+import kotlinx.android.synthetic.main.activity_settings.*
+import android.app.ActivityManager
+import android.content.Context
+
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        initView()
+    }
+
+    private fun initView() {
+
+        //storage
+        //TODO fix internal storage size (its the SDcard size now)
+        val stat: StatFs = StatFs(Environment.getExternalStorageDirectory().getPath())
+        val bytesAvailable: Long = stat.blockSizeLong * stat.blockCountLong
+        val megAvailable: Long = bytesAvailable / 1048576
+        tvStorage.text = getString(R.string.settings_max_storage, megAvailable)
+
+
+        //RAM
+        val actManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val memInfo = ActivityManager.MemoryInfo()
+        actManager.getMemoryInfo(memInfo)
+        val availableRam = memInfo.availMem / 0x100000L
+        println("HIEROO" + availableRam)
+        tvRam.text = getString(R.string.settings_ram, availableRam)
+
+
+
+
+
+
     }
 }
