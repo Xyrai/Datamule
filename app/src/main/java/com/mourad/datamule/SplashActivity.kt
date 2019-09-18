0b24af2c26461b6cb958ba9e7fd54988a0c600bb
Mourad Abouhcen <mourad@MacBook-Pro-van-Mourad.local>
2019-09-18 12:30:49 +0200

    SplashActivity and begin of the HomeActivity

---
+++ app/src/main/java/com/mourad/datamule/SplashActivity.kt
@@ -0,0 +1,49 @@
+package com.mourad.datamule
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ProgressBar
+import android.os.Handler
+
+
+
+class SplashActivity : AppCompatActivity() {
+
+
+    private val hdlr = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        var pgsBar: ProgressBar = findViewById(R.id.pBar)
+
+        var i = pgsBar.progress
+        Thread(Runnable {
+            while (i < 100) {
+                i += 1
+                // Update the progress bar and display the current value in text view
+                hdlr.post(Runnable {
+                    pgsBar.progress = i
+                })
+                try {
+                    // Sleep for 100 milliseconds to show the progress slowly.
+                    Thread.sleep(100)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+            }
+        }).start()
+
+        Handler().postDelayed({
+            startActivity(
+                Intent(
+                    this@SplashActivity,
+                    HomeActivity::class.java
+                )
+            )
+            finish()
+        }, 1000)
+    }
+}
