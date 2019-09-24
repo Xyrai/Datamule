0e7bf9a4a397d630c43d419b58f6d5e09d0ef67a
Mourad Abouhcen <mouab215@outlook.com>
2019-09-24 15:10:21 +0200

    small changes

--- app/src/main/java/com/project/datamule/UI/SearchPiActivity.kt
+++ app/src/main/java/com/project/datamule/UI/SearchPiActivity.kt
@@ -1,13 +1,27 @@
 package com.project.datamule.UI
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.datamule.R
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SearchPiActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_pi)
+
+        initView()
+    }
+
+    private fun initView() {
+        var fontAwesomeFont = Typeface.createFromAsset(getAssets(), "fa-solid-900.ttf")
+        tvBack.setTypeface(fontAwesomeFont)
+        tvBack.setOnClickListener { onClickBack() }
+    }
+
+    fun onClickBack() {
+        finish()
     }
 }
