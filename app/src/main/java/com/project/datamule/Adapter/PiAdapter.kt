d0b9526e2c911b186a636a1930f88a4b61173015
Mourad Abouhcen <mouab215@outlook.com>
2019-09-25 14:42:06 +0200

    added detailActivity for opening info about pi's

---
+++ app/src/main/java/com/project/datamule/Adapter/PiAdapter.kt
@@ -0,0 +1,32 @@
+package com.project.datamule.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.project.datamule.DataClass.Pi
+import com.project.datamule.R
+import kotlinx.android.synthetic.main.item_pi.view.*
+
+class PiAdapter (private val pi_s: List<Pi>, val clickListener: (Pi) -> Unit) : RecyclerView.Adapter<PiAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_pi, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return pi_s.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(pi_s[position], clickListener)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bind(pi: Pi, clickListener: (Pi) -> Unit) {
+            itemView.tvName.text = pi.name
+            itemView.setOnClickListener { clickListener(pi)}
+        }
+    }
+}
---
+++ app/src/main/java/com/project/datamule/UI/DetailActivity.kt
@@ -0,0 +1,27 @@
+package com.project.datamule.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.datamule.DataClass.Pi
+import com.project.datamule.R
+import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.item_pi.*
+
+const val PI_EXTRA = "PI_EXTRA"
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        initViews()
+    }
+
+    fun initViews() {
+        val pi = intent.getParcelableExtra<Pi>(PI_EXTRA)
+
+        if (pi != null) {
+            tvPiName.text = pi.name
+        }
+    }
+}
