5b89ca511dd999c81a96580c3073e347daa0f278
Nynu <NynuDev@gmail.com>
2019-10-01 11:21:46 +0200

    Changed layout of SearchPi

--- app/src/main/java/com/project/datamule/UI/SearchPiActivity.kt
+++ app/src/main/java/com/project/datamule/UI/SearchPiActivity.kt
@@ -1,10 +1,13 @@
 package com.project.datamule.UI
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.datamule.R
+import kotlinx.android.synthetic.main.activity_search_pi.*
 import kotlinx.android.synthetic.main.activity_settings.*
+import kotlinx.android.synthetic.main.activity_settings.ivBack
 
 class SearchPiActivity : AppCompatActivity() {
 
@@ -19,9 +22,16 @@ class SearchPiActivity : AppCompatActivity() {
 //        var fontAwesomeFont = Typeface.createFromAsset(getAssets(), "fa-solid-900.ttf")
 //        tvBack.setTypeface(fontAwesomeFont)
         ivBack.setOnClickListener { onClickBack() }
+        btnSearchPi.setOnClickListener { onClickOpenPiList() }
+
     }
 
-    fun onClickBack() {
+    private fun onClickOpenPiList() {
+//        val intent = Intent(this, PiListActivity::class.java)
+//        startActivity(intent)
+    }
+
+    private fun onClickBack() {
         finish()
     }
 }
