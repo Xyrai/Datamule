586b9dffedecc4402b40084f2c4b48402ea06c07
Nynu <NynuDev@gmail.com>
2019-10-01 12:49:42 +0200

    Added Pi List into SearchPiActivity

--- app/src/main/java/com/project/datamule/UI/SearchPiActivity.kt
+++ app/src/main/java/com/project/datamule/UI/SearchPiActivity.kt
@@ -4,12 +4,21 @@ import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.project.datamule.Adapter.PiAdapter
+import com.project.datamule.DataClass.Pi
 import com.project.datamule.R
 import kotlinx.android.synthetic.main.activity_search_pi.*
 import kotlinx.android.synthetic.main.activity_settings.*
 import kotlinx.android.synthetic.main.activity_settings.ivBack
+import kotlinx.android.synthetic.main.content_home.*
 
 class SearchPiActivity : AppCompatActivity() {
+
+    private var pi_s = arrayListOf<Pi>()
+    private var piAdapter = PiAdapter(pi_s, {clickedPi: Pi -> onPiClicked(clickedPi)})
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +33,29 @@ class SearchPiActivity : AppCompatActivity() {
         ivBack.setOnClickListener { onClickBack() }
         btnSearchPi.setOnClickListener { onClickOpenPiList() }
 
+
     }
 
     private fun onClickOpenPiList() {
-//        val intent = Intent(this, PiListActivity::class.java)
+        //Hide elements of Search Pi screen
+        clRectangle.visibility = View.INVISIBLE
+        btnSearchPi.visibility = View.INVISIBLE
+        tvNearbyPiTitle.visibility = View.VISIBLE
+        tvNearbyPiDesc.visibility = View.VISIBLE
+
+        //Initialize RecyclerView
+        rvSearchPi.layoutManager = LinearLayoutManager(this@SearchPiActivity, RecyclerView.VERTICAL, false)
+        rvSearchPi.adapter = piAdapter
+
+        for (i in Pi.PI_S.indices) {
+            pi_s.add(Pi(Pi.PI_S[i]))
+        }
+        piAdapter.notifyDataSetChanged()
+    }
+
+    private fun onPiClicked(clickedPi: Pi) {
+//        val intent = Intent(this, DetailActivity::class.java)
+//        intent.putExtra(PI_EXTRA, clickedPi)
 //        startActivity(intent)
     }
 
