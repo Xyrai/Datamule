aec088f75b0c023db28a7031dcad616a90312701
Mourad Abouhcen <mouab215@outlook.com>
2019-10-02 15:33:13 +0200

    finished transfer dialog

--- app/src/main/java/com/project/datamule/UI/DetailActivity.kt
+++ app/src/main/java/com/project/datamule/UI/DetailActivity.kt
@@ -1,15 +1,11 @@
 package com.project.datamule.UI
 
+import android.animation.AnimatorInflater
 import android.app.Dialog
-import android.content.DialogInterface
-import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import android.view.LayoutInflater
-import androidx.appcompat.app.AlertDialog
 import com.project.datamule.DataClass.Pi
 import com.project.datamule.R
 import kotlinx.android.synthetic.main.activity_detail.*
@@ -64,6 +60,12 @@ class DetailActivity : AppCompatActivity() {
         dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog.setCanceledOnTouchOutside(false)
         dialog.setCancelable(false)
+
+        var animatorSet = AnimatorInflater.loadAnimator(this@DetailActivity,
+            R.animator.loading_animator)
+        animatorSet.setTarget(dialog.ivTransferLoader)
+        animatorSet.start()
+
         dialog.show()
     }
 
--- app/src/main/java/com/project/datamule/UI/SplashActivity.kt
+++ app/src/main/java/com/project/datamule/UI/SplashActivity.kt
@@ -5,16 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
 import com.project.datamule.R
 import kotlinx.android.synthetic.main.activity_splash.*
-import java.util.concurrent.ThreadPoolExecutor
-
 
 class SplashActivity : AppCompatActivity() {
 
-
-    private val hdlr = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
