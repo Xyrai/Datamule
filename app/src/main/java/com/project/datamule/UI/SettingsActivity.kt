f1f51c906ee0c193032e5abc2e559fd38739e4cb
hicham <hicham_13@live.nl>
2019-10-02 15:35:27 +0200

    removed redundant code/ started with cached storage

--- app/src/main/java/com/project/datamule/UI/SettingsActivity.kt
+++ app/src/main/java/com/project/datamule/UI/SettingsActivity.kt
@@ -1,13 +1,15 @@
 package com.project.datamule.UI
 
-import android.graphics.Color
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.os.StatFs
 import kotlinx.android.synthetic.main.activity_settings.*
 import android.graphics.Typeface
+import android.net.Uri
 import com.project.datamule.R
+import java.io.File
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -34,10 +36,40 @@ class SettingsActivity : AppCompatActivity() {
         tvTotalStorage.text = getString(R.string.settings_total_storage, doubleMBToStringGB(getTotalStorageInMB()))
         tvUsed.text = doubleMBToStringGB(getUsedStorageInMB()) + " GB"
         tvFree.text = doubleMBToStringGB(getFreeStorageInMB()) + " GB"
-        //TODO fix getsize of bin folder
+
+        //temporary create folder (should be run at launch of the app)
+//        val folder = File(
+//            (Environment.getExternalStorageDirectory()).toString() +
+//            File.separator + "DataMule"
+//                )
+//
+//
+//
+//        var success = true
+//        if (!folder.exists()) {
+//        success = folder.mkdirs()
+//        }
+//        if (success) {
+//         // Do something on success
+//            println("deze")
+//            if(folder.listFiles() != null) println("HIEROO" + getfolderSize(folder))
+//        }
+//        else {
+//         // Do something else on failure
+//        }
+
+        getTempFile(this, "/testing")
+        println(getfolderSize(cacheDir))
+
+
 
         setProgressBar()
     }
+
+    private fun getTempFile(context: Context, url: String): File? =
+        Uri.parse(url)?.lastPathSegment?.let { filename ->
+            File.createTempFile(filename, null, context.cacheDir)
+        }
 
     private fun doubleMBToStringGB(mb: Double): String {
         // /1000 is for mb->gb, 100.0 is for amount of decimals (2)
@@ -63,24 +95,30 @@ class SettingsActivity : AppCompatActivity() {
         return mbFree
     }
 
+    private fun getfolderSize(directory: File): Long {
+        var length: Long = 0
+
+        for (file in directory.listFiles()) {
+            println(file.name)
+            if (file.isFile())
+                length += file.length()
+//            else
+//                length += folderSize(file)
+        }
+        return length
+    }
+
+
+
     private fun setProgressBar() {
         //1: green, 2:black 3:orange
-
-        pbStorageUsed.max = getTotalStorageInMB().toInt()
+        pbStorage.max = getTotalStorageInMB().toInt()
 
         //todo plus cache size
-//        pbStorageFree.setProgress((getFreeStorageInMB() * 0.5).toInt())
-//
-//
-//
-//        pbStorageCache.setProgress((getUsedStorageInMB() + getFreeStorageInMB() * 0.5).toInt())
+        var cache = (getFreeStorageInMB() * 0.02).toInt() + getUsedStorageInMB().toInt()
 
-        var cache = (getUsedStorageInMB() + getFreeStorageInMB() * 0.5).toInt()
-
-//        pbStorageUsed.setProgress(getUsedStorageInMB().toInt())
-        pbStorageUsed.progress = getUsedStorageInMB().toInt()
-//        pbStorageUsed.
-        pbStorageUsed.secondaryProgress = cache
+        pbStorage.progress = getUsedStorageInMB().toInt()
+        pbStorage.secondaryProgress = cache
     }
 
 
