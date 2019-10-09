4c5c662976ffac1edd4a720f29fbd9c023b7f54c
hicham <hicham_13@live.nl>
2019-10-09 14:19:52 +0200

    started on pushnotification

--- app/src/main/java/com/project/datamule/Constants.kt
+++ app/src/main/java/com/project/datamule/Constants.kt
@@ -4,6 +4,7 @@ class Constants {
     companion object {
         val REQUEST_ENABLE_BT = 9001
         val REQUEST_PHONE_CALL = 9002
+        val CHANNEL_ID = "DataMule"
 
         val DATAMULE_PHONE_NUMBER = "+31612345678"
         val DATAMULE_EMAIL = "datamule@atos.com"
--- app/src/main/java/com/project/datamule/UI/SettingsActivity.kt
+++ app/src/main/java/com/project/datamule/UI/SettingsActivity.kt
@@ -2,6 +2,9 @@ package com.project.datamule.UI
 
 import android.Manifest
 import android.app.Dialog
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -22,6 +25,10 @@ import com.project.datamule.R
 import java.io.File
 import androidx.core.app.ActivityCompat
 import android.content.pm.PackageManager
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 
 //1 MB = 1048576 bytes (1024 bytes * 1024 KB = 1048576 bytes = 1MB)
@@ -32,6 +39,7 @@ class SettingsActivity : AppCompatActivity() {
     private lateinit var fontAwesomeFont: Typeface
 
 
+    //TODO set setttingsactivity scrollable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -46,11 +54,44 @@ class SettingsActivity : AppCompatActivity() {
         clChangeLog.setOnClickListener { buildChangeLogDialog() }
         clSupport.setOnClickListener { buildSupportDialog() }
         clPushNotification.setOnClickListener {
-            if(sPushNotification.isChecked) sPushNotification.setChecked(false)
-            else sPushNotification.setChecked(true)
+            if(sPushNotification.isChecked) {
+                sPushNotification.setChecked(false)
+                setPushNotification(false)
+            } else {
+                sPushNotification.setChecked(true)
+                setPushNotification(true)
+            }
         }
         createCacheFile()
         setStorage()
+    }
+
+    private fun setPushNotification(allowPushNotification: Boolean) {
+
+        var builder = NotificationCompat.Builder(this, Constants.CHANNEL_ID)
+            .setSmallIcon(R.drawable.logo_no_text)
+            .setContentTitle("Wassup")
+            .setContentText("kech")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+//        with(NotificationManagerCompat.from(this)) {
+//            // notificationId is a unique int for each notification that you must define
+//            notify(1, builder.build())
+//        }
+
+        val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+//        val defaultSoundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+////        val notificationBuilder = NotificationCompat.Builder(this)
+////            .setSmallIcon(R.drawable.logo_no_text)
+////            .setContentTitle("title")
+////        .setContentText("message")
+////            .setAutoCancel(true)
+////            .setSound(defaultSoundUri)
+
+        notificationManager.notify(6, builder.build())
+
     }
 
     private fun buildChangeLogDialog() {
