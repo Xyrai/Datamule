e0ea16a40c6cf2698a3dd94b78e4a54c3b31d134
Nynu <NynuDev@gmail.com>
2019-12-10 15:36:50 +0100

    Fixed background service

--- app/src/main/java/com/project/datamule/Utils/IntentService.kt
+++ app/src/main/java/com/project/datamule/Utils/IntentService.kt
@@ -6,12 +6,22 @@ import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.IntentFilter
 import android.net.wifi.WifiManager
+import android.os.IBinder
+import android.util.Log
+import java.lang.Thread.sleep
+import java.text.SimpleDateFormat
+import java.util.*
+import android.R
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.app.NotificationCompat
 
 
-class IntentService : IntentService("BackgroundService") {
+class IntentService : Service() {
     private var wifiStateReceiver: BroadcastReceiver? = null
 
-    override fun onHandleIntent(intent : Intent) {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
     }
 
     override fun onCreate() {
@@ -26,12 +36,25 @@ class IntentService : IntentService("BackgroundService") {
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        onHandleIntent(intent)
-        return Service.START_STICKY
+        var date = Date();
+        val formatter = SimpleDateFormat("MMM dd yyyy HH:mm:ssa")
+        val answer: String = formatter.format(date)
+        Log.e("START", "START START START $answer")
+        //TODO: Adjust notification
+        val notification = NotificationCompat.Builder(this, "1")
+            .setContentTitle("title")
+            .setContentText("text")
+            .build()
+        startForeground(1, notification)
+        return START_STICKY
     }
 
     override fun onDestroy() {
+        var date = Date();
+        val formatter = SimpleDateFormat("MMM dd yyyy HH:mm:ssa")
+        val answer: String = formatter.format(date)
         unregisterReceiver(wifiStateReceiver)
+        Log.e("DESTROY", "DESTROY DESTROY DESTROY $answer")
         super.onDestroy()
     }
 }
